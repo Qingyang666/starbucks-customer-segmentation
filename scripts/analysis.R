b5340dfb7210d8ab7811cdf9bf652b3ccfe40a29
Qingyang666 <liqingyang@ris-MacBook-Pro.local>
2021-07-26 11:51:39 -0400

    Update analysis.R

--- scripts/analysis.R
+++ scripts/analysis.R
@@ -1,6 +1,8 @@
 library(tidyverse)
 library(ggplot2)
 library(NbClust)
+library(factoextra)
+library(cluster)
 # import final data set
 starbucks_customer <- read_csv('data/profile_offer_transaction.csv')[,-1]
 starbucks_customer <- starbucks_customer %>%
@@ -66,8 +68,8 @@ ggplot(data = importance_of_components,
        subtitle = 'Cumulative Proportion of Variance Explained')+
   theme(text = element_text(family = "serif", size = 10))
 
-# I want to choose 12 principal components, which explains 91% of the variance.
-model_df <- as_tibble(predictors_scale_pca$x[,1:12])
+# I want to choose 11 principal components, which explains 95% of the variance.
+model_df <- as_tibble(predictors_scale_pca$x[,1:14])
 
 ##############################CLUSTER ANALYSIS (PART1)##########################
 
@@ -100,14 +102,14 @@ clust.ward <- hclust(dist.mat, method = 'ward.D2')
 plot(clust.ward, hang = -1, main = "Ward Linkage", labels = FALSE, xlab = "", sub = "")
 
 # Ward method derive the best separation between each cluster. 
-# Then we need to determine the optimal number of clusters.
+# Then we need to determine the optimal number of clusters using.
 ward.silhouette <- NbClust(data = model_df, 
                     distance = "euclidean",
                     min.nc = 2, 
-                    max.nc = 14, 
+                    max.nc = 10, 
                     method = 'ward.D2',
                     index = "silhouette")
-# visualization
+# visualization 
 ggplot(data = data.frame(num_clusters = as.numeric(names(ward.silhouette$All.index)),
                          avg_silhouette = ward.silhouette$All.index),
        aes(x = num_clusters,
@@ -115,21 +117,102 @@ ggplot(data = data.frame(num_clusters = as.numeric(names(ward.silhouette$All.ind
            group = 1)) +
   geom_point(size = 0.8, color = 'steelblue')+
   geom_line(color = 'steelblue') +
-  geom_hline(yintercept = 0.0779,linetype="dashed", color = "grey") +
-  geom_vline(xintercept = 4, linetype="dashed", color = "grey") +
-  scale_x_continuous(name = 'number of clusters', breaks = c(2:14))+
+  geom_hline(yintercept = 0.0774,linetype="dashed", color = "grey") +
+  geom_vline(xintercept = 3, linetype="dashed", color = "grey") +
+  scale_x_continuous(name = 'number of clusters', breaks = c(2:10))+
   scale_y_continuous(name = 'average silhouette width')+
   labs(title = 'Optimal Number of Clusters',
        subtitle = 'silhouette method')+
   theme(text = element_text(family = "serif", size = 10))
+
 # I would like to choosing local maximum value as my optimal number of clusters, which is 4 clusters.
+ward_cluster_3 <- cutree(clust.ward, k = 3)
+
+# visualize clusters in first two principal component space
+theme_set(theme_minimal())
+ward_pc <- model_df %>% mutate(cluster = as.factor(ward_cluster_3))
+ggplot(data = ward_pc,
+       aes(x = PC1,
+           y = PC2, 
+           color = cluster,
+           shape = cluster))+
+  geom_point(size = 0.7, alpha = 0.6)+ 
+  geom_hline(yintercept=0,color="grey", linetype="dashed")+
+  geom_vline(xintercept=0,color="grey", linetype="dashed")+
+  labs(title="Ward (D2) Linkage In Pricnipal Components Space",
+       x="PC1", y = "PC2")+
+  theme(plot.title = element_text(face = 'bold', size = 10, hjust = 0.5))
+
+##############################CLUSTER ANALYSIS (PART2)##########################
+
+# using elbow method
+set.seed(2021)
+tot_wss <- c()
+k.levels <- 1:10
+for(k in k.levels){
+  clust.km <- kmeans(x = model_df, centers = k, nstart = 100, iter.max = 100)
+  tot_wss[k]<-clust.km$tot.withinss
+}
+# visualization
+as_tibble(tot_wss) %>%
+  mutate(num_clusters = as.factor(k.levels)) %>%
+  ggplot(aes(x = num_clusters, 
+             y = tot_wss, 
+             group = 1)) +
+  geom_line(color = "steelblue")+
+  geom_point(color = "steelblue")+
+  labs(title = "Optimal Number of Clusters",
+       subtitle = "Elbow Method",
+       x = "Number of clusters",
+       y = "Total Within Sum of Squares")
+
+# not very clear, maybe 5 clusters 
+
+# using silhouetee score
+km.silhouette <- NbClust(data = model_df, 
+                           distance = "euclidean",
+                           min.nc = 2, 
+                           max.nc = 10, 
+                           method = 'kmeans',
+                           index = "silhouette")
+
+# visualization
+ggplot(data = data.frame(num_clusters = as.numeric(names(km.silhouette$All.index)),
+                         avg_silhouette = km.silhouette$All.index),
+       aes(x = num_clusters,
+           y = avg_silhouette,
+           group = 1)) +
+  geom_point(size = 0.8, color = 'steelblue')+
+  geom_line(color = 'steelblue') +
+  geom_hline(yintercept = 0.1150,linetype="dashed", color = "grey") +
+  geom_vline(xintercept = 4, linetype="dashed", color = "grey") +
+  scale_x_continuous(name = 'number of clusters', breaks = c(2:10))+
+  scale_y_continuous(name = 'average silhouette width')+
+  labs(title = 'Optimal Number of Clusters',
+       subtitle = 'silhouette method')+
+  theme(text = element_text(family = "serif", size = 10))
+
+set.seed(2021)
+cluster_km_3 <- kmeans(x = model_df, centers = 3, nstart = 100, iter.max = 100)
+plot(cluster::silhouette(cluster_km_3$cluster, dist.mat), col=1:3, border=NA)
+
+kmeans_pc <- model_df %>% 
+  mutate(cluster = as.factor(cluster_km_3$cluster))
+ggplot(data = kmeans_pc,
+       aes(x = PC1,
+           y = PC2, 
+           color = cluster,
+           shape = cluster))+
+  geom_point(size = 0.7, alpha = 0.6)+ 
+  geom_hline(yintercept=0,color="grey", linetype="dashed")+
+  geom_vline(xintercept=0,color="grey", linetype="dashed")+
+  labs(title="Kmeans In Pricnipal Components Space",
+       x="PC1", y = "PC2")+
+  theme(plot.title = element_text(face = 'bold', size = 10, hjust = 0.5))
 
 # add back clusters to our original data set
-cluster_4 <- cutree(clust.ward, k = 4)
 starbucks_customer_cluster <-  customer_id_df %>%
-  mutate(cluster = as.factor(cluster_4)) %>%
+  mutate(cluster = as.factor(cluster_km_3$cluster)) %>%
   left_join(starbucks_customer, by = 'customer_id')
 
 write.csv(starbucks_customer_cluster, 'data/starbucks_customer_cluster.csv')
-
-
